b1477c508c298cf921791d05f6e6859d6898ae65
Toon Willems <m@toonwillems.be>
2018-07-18 09:11:09 +0000

    implement string decoding and add tests

--- src/parser/decoding.rs
+++ src/parser/decoding.rs
@@ -18,8 +18,28 @@ impl Decode for String {
     fn decode<R: Read>(reader: &mut R, _: &mut Self::DecoderState) -> Result<Self, FromUtf8Error> {
         let len = reader.read_u16::<BigEndian>().unwrap();
         let mut buf = Vec::with_capacity(len as usize);
-        reader.read_exact(&mut buf).expect("Could not read expected amount of bytes");
+        reader.take(len as u64).read_to_end(&mut buf).expect("Could not read expected amount of bytes");
         String::from_utf8(buf)
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+    #[test]
+    fn it_decodes_empty_string_values() {
+        let mut cursor = Cursor::new(vec![0,0]);
+        let decoded_string = String::decode(&mut cursor, &mut ()).unwrap();
+        assert_eq!(decoded_string, String::new());
+    }
+
+    #[test]
+    fn it_decodes_string_values() {
+        let mut data: Vec<u8> = vec![0,7];
+        data.extend_from_slice("testing".as_bytes());
+        let mut cursor = Cursor::new(data);
+        let decoded_string = String::decode(&mut cursor, &mut ()).unwrap();
+        assert_eq!(decoded_string, String::from("testing"));
+    }
+}
